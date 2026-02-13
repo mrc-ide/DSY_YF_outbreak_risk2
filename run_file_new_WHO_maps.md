a07521cb9b082436d9c4a3fbf1e32f6225513311
KeithJF82 <keith.fraser@imperial.ac.uk>
2026-02-13 18:29:17 +0000

    Adding src files to make maps with WHO shapefiles

---
+++ run_file_new_WHO_maps.md
@@ -0,0 +1,30 @@
+# Initialize
+path=getwd() #Adjust if not using project file
+orderly2::orderly_init(path)
+library(YEPaux)
+
+# Calculate FOI/R0 values from Markov chain parameter outputs and environmental data
+orderly2::orderly_run("get_FOI_R0_values_from_saved_chain_data",
+  list(n_param_sets=1000,enviro_filename="enviro_data_IAregions_6covs_new_labelling.csv",
+  chain_filename="markov_chain_data_combined_paper_latest.Rds"))
+
+# Create maps of FOI and R0 values
+orderly2::orderly_run("map_FOI_R0_values_WHO")
+
+# Calculate case data based on seeded case and R0 values
+orderly2::orderly_run("case_data_calc_R0_case_seeding",
+                      list(p_severe_inf=0.12, p_death_severe_inf=0.39, deterministic=FALSE,n_sets_to_run=10,n_reps=10,
+                           mode_parallel=TRUE,n_cores=4))
+
+# Calculate outbreak risk from second case data set
+orderly2::orderly_run("get_outbreak_risk_R0_case_seeding")
+
+# Calculate outbreak risk based on seeding weighted by Raptor data
+orderly2::orderly_run("outbreak_risk_seeded_weighted_by_raptor_data",
+  list(raptor_results_filename="all_DS_results_neighbours 1.rds"))
+  
+# Map attack rate and other alternate infection/outbreak outputs based on seeding
+orderly2::orderly_run("maps_create_WHO")
+
+# Create violin plot of R0 values
+orderly2::orderly_run("R0_violin_plot")
--- src/map_FOI_R0_values_WHO/map_FOI_R0_values_WHO.R
+++ src/map_FOI_R0_values_WHO/map_FOI_R0_values_WHO.R
@@ -3,22 +3,9 @@
 orderly2::orderly_dependency(name="get_FOI_R0_values_from_saved_chain_data", query="latest", #TODO - make query input parameter
                              files=c(DSY_selected_datasets_FOI_R0.Rds="DSY_selected_datasets_FOI_R0.Rds"))
 
-
-orderly2::orderly_shared_resource('shapefiles/DJI/gadm36_DJI_1.cpg' = 'shapefiles/DJI/gadm36_DJI_1.cpg', 
-                                  'shapefiles/DJI/gadm36_DJI_1.dbf' = 'shapefiles/DJI/gadm36_DJI_1.dbf', 
-                                  'shapefiles/DJI/gadm36_DJI_1.prj' = 'shapefiles/DJI/gadm36_DJI_1.prj', 
-                                  'shapefiles/DJI/gadm36_DJI_1.shp' = 'shapefiles/DJI/gadm36_DJI_1.shp', 
-                                  'shapefiles/DJI/gadm36_DJI_1.shx' = 'shapefiles/DJI/gadm36_DJI_1.shx', 
-                                  'shapefiles/SOM/gadm36_SOM_1.cpg' = 'shapefiles/SOM/gadm36_SOM_1.cpg', 
-                                  'shapefiles/SOM/gadm36_SOM_1.dbf' = 'shapefiles/SOM/gadm36_SOM_1.dbf', 
-                                  'shapefiles/SOM/gadm36_SOM_1.prj' = 'shapefiles/SOM/gadm36_SOM_1.prj',
-                                  'shapefiles/SOM/gadm36_SOM_1.shp' = 'shapefiles/SOM/gadm36_SOM_1.shp', 
-                                  'shapefiles/SOM/gadm36_SOM_1.shx' = 'shapefiles/SOM/gadm36_SOM_1.shx', 
-                                  'shapefiles/YEM/gadm36_YEM_1.cpg' = 'shapefiles/YEM/gadm36_YEM_1.cpg', 
-                                  'shapefiles/YEM/gadm36_YEM_1.dbf' = 'shapefiles/YEM/gadm36_YEM_1.dbf', 
-                                  'shapefiles/YEM/gadm36_YEM_1.prj' = 'shapefiles/YEM/gadm36_YEM_1.prj', 
-                                  'shapefiles/YEM/gadm36_YEM_1.shp' = 'shapefiles/YEM/gadm36_YEM_1.shp', 
-                                  'shapefiles/YEM/gadm36_YEM_1.shx' = 'shapefiles/YEM/gadm36_YEM_1.shx')
+#Load new shape data and region cross-referencing table
+orderly2::orderly_shared_resource('shapefile_data_DSY_adm1.Rds' = 'shapefile_data_DSY_adm1.Rds',
+                                  'xref_adm1.Rds' = 'xref_adm1.Rds')
 
 #library(YEPaux)
 country_list=c("DJI","SOM","YEM")
@@ -30,11 +17,8 @@ FOI_R0_dist_data[,c(3:8)]=FOI_R0_dist_data[,c(3:8)]*365.0 #Convert daily FOI to 
 regions=FOI_R0_dist_data$region
 country_list=unique(substr(regions,1,3))
 
-shapefiles=rep("",length(country_list))
-for(i in 1:length(country_list)){
-  shapefiles[i]=paste0("shapefiles/",country_list[i],"/gadm36_",country_list[i],"_1.shp")
-}
-shape_data=YEPaux::map_shapes_load(regions, shapefiles, region_label_type="GID_1")
+shape_data=readRDS("shapefile_data_DSY_adm1.Rds")
+xref_table=readRDS("xref_adm1.Rds")
 
 colour_scale=readRDS(file=paste(path.package("YEPaux"), "exdata/colour_scheme_example.Rds", sep="/"))$colour_scale
 #scale_FOI=pretty(as.vector(t(FOI_R0_dist_data[,c(3:6)])),10)
@@ -49,21 +33,22 @@ for(data_select in c(1,2,3,4,5)){
   FOI_values=FOI_R0_dist_data[,2+data_select]
   R0_values=FOI_R0_dist_data[,9+data_select]
   
+  #TODO - remap FOI/R0 values onto WHO regions
+  #FOI_values2
+  #R0_values2
+  
   orderly2::orderly_artefact(description=paste0("Figures ",data_select), files=c(paste0("epi_map_FOI_",data_type,".png"),
                                                                                  paste0("epi_map_R0_",data_type,".png")))
   
-  
   png(paste0("epi_map_FOI_",data_type,".png"),width=945.507,height=1440)
-  YEPaux::create_map(shape_data,FOI_values,scale=scale_FOI,colour_scale,pixels_max=1440,
+  YEPaux::create_map(shape_data,FOI_values2,scale=scale_FOI,colour_scale,pixels_max=1440,
                      text_size=2,map_title="",legend_title="Spillover force of infection (annual)",legend_position="bottomright",
                      legend_format="e",legend_dp=1,output_file=NULL)
   dev.off()
   
   png(paste0("epi_map_R0_",data_type,".png"),width=945.507,height=1440)
-  YEPaux::create_map(shape_data,R0_values,scale=scale_R0,colour_scale,pixels_max=1440,
+  YEPaux::create_map(shape_data,R0_values2,scale=scale_R0,colour_scale,pixels_max=1440,
                      text_size=2,map_title="",legend_title="Basic reproduction number",legend_position="bottomright",
                      legend_format="f",legend_dp=2,output_file=NULL)
   dev.off()
-  
-  
 }
